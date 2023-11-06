a1101d9469991ec030e8bd7426623eacbecf24fe
Ratna Amalia Azzahra <amaliaratna41@gmail.com>
2023-11-06 14:35:35 +0700

    first commit

---
+++ resources/views/linktree.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tenjinscrollworks | Instagram | Linktree</title>
+    <link rel="stylesheet" href="{{ asset('css/linktree.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="icon" href="https://seeklogo.com/images/L/linktree-logo-6FC3ADB679-seeklogo.com.png">
+</head>
+<body>
+    <header>
+        <img src="https://ugc.production.linktr.ee/egtm7hKuR3SJljafa4iP_y4fs8AD230YXYoKD" alt="">
+        <h1>@tenjinscrollworks</h1>
+        <h2>Get your gears ready, join the forces.</h2>
+    </header>
+    <div class="amel-container">
+        <div class="amel-links">
+            <ul>
+                <li>
+                    <a href="https://www.tokopedia.com/tenjinstyle" target="_blank">
+                        <span>🛒 Tokopedia Store Page (Indonesia) </span>
+                        <i class="fas fa-ellipsis"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.tenjin.style/" target="_blank">
+                        <span>🌏 Web Store (International) </span>
+                        <i class="fas fa-ellipsis"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://discord.io/tenjinscrollworks" target="_blank">
+                        <span>💬 Discord Community </span>
+                        <i class="fas fa-ellipsis"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.google.com/maps/place/Tenjin+Style+%26+Scrollworks/@-6.2605912,106.6295331,16z/data=!4m6!3m5!1s0x2e69fd57e4a2b163:0x91fac4150d2224b6!8m2!3d-6.2607411!4d106.6291523!16s%2Fg%2F11js8934dk?entry=ttu" target="_blank">
+                        <span>🏬 Offline Store</span> 
+                        <i class="fas fa-ellipsis"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://open.spotify.com/playlist/2lOiHFAZWhYYoQZRg6aShi?si=3f22c06767fa45fd&utm_medium=share&utm_source=linktree&nd=1" target="_blank">
+                        <span>🎵 Tenjin Collective Tunes </span>
+                        <i class="fas fa-ellipsis"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/watch?v=-d0wSeECdXo" target="_blank">
+                        <span>🧼 Cara Mencuci Deskmat </span>
+                        <i class="fas fa-ellipsis"></i>
+                        
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="amel-menu-btn">
+            <div class="amel-menu-circle">
+                <i class="fas fa-ellipsis"></i>
+            </div>
+        </div>
+        <div class="amel-icon">
+            <div class="amel-ig-icon">
+                <a href="https://www.instagram.com/tenjin.scrollworks/" target="_blank">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>    
+            </div>
+            <div class="amel-linktree-icon">
+                <a href="https://linktr.ee/?utm_source=linktree&utm_medium=profile&utm_content=tenjinscrollworks">
+                    <img src="https://www.logggos.club/logos/linktree.svg" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,81 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+    return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
+});
+
+Route::get('halo2', function () {
+    return "<marquee>Halo, Selamat datang</marquee>";
+});
+
+Route::get('blog', function () {
+    return view('blog');
+});
+
+Route::get('hello', function () {
+    return view('hello');
+});
+
+Route::get('pertemuan1', function () {
+    return view('pertemuan1');
+});
+
+Route::get('berita', function () {
+    return view('berita');
+});
+
+Route::get('index', function () {
+    return view('index');
+});
+
+Route::get('pertemuan3', function () {
+    return view('pertemuan3');
+});
+
+Route::get('linktree', function () {
+    return view('linktree');
+});
+
+Route::get('payment', function () {
+    return view('payment');
+});
+
+Route::get('js1', function () {
+    return view('js1');
+});
+
+Route::get('js2', function () {
+    return view('js2');
+});
+
+Route::get('validasi1', function () {
+    return view('validasi1');
+});
+
+Route::get('tugas2', function () {
+    return view('tugas2');
+});
+
+Route::get('news', function () {
+    return view('news');
+});
+
+
+
